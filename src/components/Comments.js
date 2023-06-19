7278eaf5f090969798df324666626476ea054a1b
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-06-19 22:51:27 +0300

    add initials to every comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -26,9 +26,13 @@ const Comments = () => {
       <h2>Comments:</h2>
       {comments.map((comment, index) => (
         <div key={index} className="comment">
-          <p className="comment-name">
-            {comment.user?.username || comment.name}
-          </p>
+          <p className="user-name">{comment.user?.username || comment.name}</p>
+          <div className="user-logo">
+            <span className="initials">
+              {comment.user?.username.charAt(0).toUpperCase() ||
+                comment.name.charAt(0).toUpperCase()}
+            </span>
+          </div>
           <p className="comment-body">{comment.body}</p>
           <button onClick={() => handleDeleteComment(index)}>Delete</button>
           <hr className="comment-divider" />
